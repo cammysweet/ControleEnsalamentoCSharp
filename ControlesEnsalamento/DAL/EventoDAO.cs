a383cbacbe7cf9ecd947aadaa08b305fcb05e5a2
Camila Cardoso <camila.cardoso65@hotmail.com>
2017-04-04 13:43:03 -0300

    implementação da DAO e começo do calendário

---
+++ ControlesEnsalamento/DAL/EventoDAO.cs
@@ -0,0 +1,34 @@
+﻿using ControlesEnsalamento.Models;
+using System;
+using System.Collections.Generic;
+
+namespace ControlesEnsalamento.DAL
+{
+    public class EventoDAO
+    {
+
+        public List<Evento> ListarEventos(DateTime dataInicial, DateTime dataFinal)
+        {
+            //TODO implementar método para listar eventos
+            throw new NotImplementedException();
+        }
+
+        public void Adicionar(Evento evento)
+        {
+            //TODO implementar método para salvar um evento
+            throw new NotImplementedException();
+        }
+
+        public Evento ConsultarPorId(int id)
+        {
+            //TODO implementar método para consultar eventos por id
+            throw new NotImplementedException();
+        }
+
+        public void Atualizar(Evento evento)
+        {
+            //TODO implementar método para atualizar um evento
+            throw new NotImplementedException();
+        }
+    }
+}
